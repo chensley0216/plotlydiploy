8e2e41b82937fb2e0458229b9d7085ebeba99b2c
chensley0216 <100445222+chensley0216@users.noreply.github.com>
2022-05-30 22:49:08 -0400

    Debugged challenge

--- static/javascript/charts.js
+++ static/javascript/charts.js
@@ -99,6 +99,60 @@ function buildCharts(sample) {
 
 // Deliverable 2: Bubble Chart
 
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  d3.json("static/javascript/data/samples.json").then((data) => {
+    var sampleNames = data.names;
+
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    var firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+}
+
+// Initialize the dashboard
+init();
+
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildMetadata(newSample);
+  buildCharts(newSample);
+  
+}
+
+// Demographics Panel 
+function buildMetadata(sample) {
+  d3.json("static/javascript/data/samples.json").then((data) => {
+    var metadata = data.metadata;
+    // Filter the data for the object with the desired sample number
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    // Use d3 to select the panel with id of `#sample-metadata`
+    var PANEL = d3.select("#sample-metadata");
+
+    // Use `.html("") to clear any existing metadata
+    PANEL.html("");
+
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+    Object.entries(result).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+
+  });
+}
     // 1. Create the trace for the bubble chart.
     let samples = data.samples;
     let sampleNumbers = samples.filter(sampleObj => sampleObj.id == sample);
@@ -133,11 +187,66 @@ function buildCharts(sample) {
     Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
 
 
+    function init() {
+      // Grab a reference to the dropdown select element
+      var selector = d3.select("#selDataset");
+    
+      // Use the list of sample names to populate the select options
+      d3.json("static/javascript/data/samples.json").then((data) => {
+        var sampleNames = data.names;
+    
+        sampleNames.forEach((sample) => {
+          selector
+            .append("option")
+            .text(sample)
+            .property("value", sample);
+        });
+    
+        // Use the first sample from the list to build the initial plots
+        var firstSample = sampleNames[0];
+        buildCharts(firstSample);
+        buildMetadata(firstSample);
+      });
+    }
+    
+    // Initialize the dashboard
+    init();
+    
+    function optionChanged(newSample) {
+      // Fetch new data each time a new sample is selected
+      buildMetadata(newSample);
+      buildCharts(newSample);
+      
+    }
+    
+    // Demographics Panel 
+    function buildMetadata(sample) {
+      d3.json("static/javascript/data/samples.json").then((data) => {
+        var metadata = data.metadata;
+        // Filter the data for the object with the desired sample number
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+        var result = resultArray[0];
+        // Use d3 to select the panel with id of `#sample-metadata`
+        var PANEL = d3.select("#sample-metadata");
+    
+        // Use `.html("") to clear any existing metadata
+        PANEL.html("");
+    
+        // Use `Object.entries` to add each key and value pair to the panel
+        // Hint: Inside the loop, you will need to use d3 to append new
+        // tags for each key-value in the metadata.
+        Object.entries(result).forEach(([key, value]) => {
+          PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+        });
+    
+      });
+    }
+
 // Deliverable 3: Gauge Chart
 // Create the buildChart function.
 function buildCharts(sample) {
   // Use d3.json to load the samples.json file 
-  d3.json("samples.json").then((data) => {
+  d3.json("static/javascript/data/samples.json").then((data) => {
     console.log(data);
 
     // Create a variable that holds the samples array. 
@@ -171,13 +280,13 @@ function buildCharts(sample) {
 
     // 4. Create the trace for the gauge chart.
     var gaugeData = [
-     value: washingFrequency,
-     type: "indicator",
-     mode: "gauge+number",
-     title: "Washing Frequency",
-     gauge: {
-       axis: {range: [null,10], dtick: "2"}
-       bar: {color: "black"}
+     value = washingFrequency,
+     type = "indicator",
+     mode = "gauge+number",
+     title = "Washing Frequency",
+     gauge = {
+       axis: {range: [null,10], dtick: "2"},
+       bar: {color: "black"},
        steps: [
         {range: [0, 2], color: "red"},
         {range: [2, 4], color: "orange"},
